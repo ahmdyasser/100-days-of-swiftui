191ba2a0158ec4570d4abcccbff30b38c4aed32e
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-03-26 00:15:10 +0200

    Initial Commit

---
+++ 100-days-of-swiftui/_00_days_of_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00_days_of_swiftuiApp.swift
+//  100-days-of-swiftui
+//
+//  Created by Ahmad Yasser on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct _00_days_of_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
