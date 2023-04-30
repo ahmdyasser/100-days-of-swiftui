eeb8315958a795332a409fde301a448c33715d9d
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-04-30 17:53:59 +0300

    Complete day 16

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State private var students = ["ahmd", "harry", "ron"]
+  @State private var selectedStudent = ""
+  var body: some View {
+    Form {
+      Picker("Student", selection: $selectedStudent) {
+        ForEach(students, id: \.self) {
+          Text($0)
         }
-        .padding()
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
