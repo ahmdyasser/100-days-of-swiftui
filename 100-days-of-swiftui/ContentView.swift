3a4dc295ddf85f1b69afbae4cdc410f776b9ea2e
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-04-30 17:06:53 +0300

    Add Tagged package

--- 100-days-of-swiftui/ContentView.swift
+++ 100-days-of-swiftui/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var configureIsTapped = false
     @StateObject var viewModel = ViewModel()
-    
+
     var body: some View {
         NavigationView {
 
