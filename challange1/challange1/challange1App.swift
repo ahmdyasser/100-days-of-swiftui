3e00ef59c4c80d485ac82cd8cc8de87760373fce
Ahmad Yasser <ahmadyas_ser@icloud.com>
2023-05-07 22:53:47 +0300

    Finish challange1

---
+++ challange1/challange1/challange1App.swift
@@ -0,0 +1,17 @@
+//
+//  challange1App.swift
+//  challange1
+//
+//  Created by Ahmad Yasser on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct challange1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
